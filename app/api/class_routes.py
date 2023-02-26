f6aba26682cc178d007ef873a30f0d2221afca82
David Pratt <david@midnightmechanism.com>
2023-02-26 12:39:36 -0800

    Finish 'Create a class' route

--- app/api/class_routes.py
+++ app/api/class_routes.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, jsonify, session, request
-from app.models import User, db, Class
+from app.models import User, db, Class, CategoryClass
 from flask_login import current_user, login_user, logout_user, login_required
 
 class_routes = Blueprint('class', __name__)
 
+### get all classes
 @class_routes.route('/', methods = ["GET"])
 def get_all_classes():
     if current_user.is_authenticated:
@@ -13,3 +14,38 @@ def get_all_classes():
     all_classes = [ class_var.to_dict() for class_var in classes ] # class is a reserved keyword
     
     return {"All Classes": all_classes}
+
+### create a class
+@class_routes.route('/', methods = ["POST"])
+def create_classes():
+    if current_user.is_authenticated:
+        
+        data = request.json
+        
+        new_class = Class(
+            name = data['name'],
+            user_id = 4 # data['user_id']
+        )
+        
+        db.session.add(new_class)
+        db.session.commit()
+        
+        result = {
+            "id": new_class.id,
+            "name": new_class.name,
+            "user_id": new_class.user_id
+        }
+    
+    return {"New Class": result }
+
+### delete a class
+# @class_routes.route('/<int:id>', methods = ["DELETE"])
+# def delete_class_by_id(id):
+#     if current_user.is_authenticated:
+#         class_obj = Class.query.get(id)
+#         category_classes = CategoryClass.query.filter(CategoryClass.class_id == 1).all()
+#         db.session.delete(class_obj)
+#         db.session.delete(category_classes[0])
+#         db.session.commit()
+#         print("GoOOD")
+#     return 'Hello world!'
--- migrations/versions/bdf8e23db563_.py
+++ migrations/versions/bdf8e23db563_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 235e4923f1b1
+Revision ID: bdf8e23db563
 Revises: 
-Create Date: 2023-02-25 10:24:51.989601
+Create Date: 2023-02-25 16:48:35.334352
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '235e4923f1b1'
+revision = 'bdf8e23db563'
 down_revision = None
 branch_labels = None
 depends_on = None
