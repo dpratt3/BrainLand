3ddc4cd5ec881093ff54c8642f96d4e6ca4e5440
David Pratt <david@midnightmechanism.com>
2023-03-01 17:15:46 -0800

    Style login/signup form

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -13,7 +13,7 @@ function HomePage() {
   return (
       <div><div style={{ display: "flex", justifyContent: "center", marginTop: 8 }}>
           <img src="https://miro.medium.com/v2/resize:fit:828/0*Bx8KWmQYWfJN0_w5"
-              style={{ width: 800, height: 450, borderRadius: 4, opacity: 0.85 }} alt="crowd of listeners" />
+              style={{ width: 800, height: 450, borderRadius: 4, opacity: 0.65 }} alt="crowd of listeners" />
       </div>
           <div style={{ textAlign: "center", backgroundColor: "#36013F", marginTop: 8, color: "#fff" }}>
               BrainScape Clone | Author : David Pratt
